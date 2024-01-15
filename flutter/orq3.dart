5a7735c3c701ec84ada9902fd76257a9dc46b570
Dxny <dany@delacruz.pe>
2024-01-14 23:39:13 -0500

    Dart

---
+++ flutter/orq3.dart
@@ -0,0 +1,19 @@
+/**
+// Color en elevatedbutton icon
+// https://www.flutterbeads.com/elevated-button-color-in-flutter/
+
+ElevatedButton(
+  onPressed: () {},
+  style: ElevatedButton.styleFrom(
+    foregroundColor : Colors.white ,
+    backgroundColor : Colors.green.shade300 , // Text Color (Foreground color),
+    fixedSize       : const Size(150, 70)
+  ),
+  child: const Text(
+    'Elevated Button',
+    style: TextStyle(fontSize: 40),
+  ),
+)
+
+
+*/
