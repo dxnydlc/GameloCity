3e784de7b7fb3fa8b10ad23a88c588a47b341560
Dany DLC <dany@delacruz.pe>
2024-02-08 11:54:16 -0500

    Nest

--- Nest/Servicio_Nest.ts
+++ Nest/Servicio_Nest.ts
@@ -1,8 +1,80 @@
 // Dxny DLC - 07/02/2023
 
+/**
+ * 
+// ========================== ENTITIES
+
+
+import { ClienteModel } from "src/clientes/model/cliente_schema.entity"
+import { SucursalModel } from "src/sucursales/model/sucursal_schema.entity"
+import { Column, Entity, Generated, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm"
+
+
+@Entity({ name: 'orq_datos' })
+export class DatosModel {
+
+    @PrimaryGeneratedColumn() 
+    id : number
+
+    @Column()
+    @Generated("uuid")
+    uu_id : string
+
+    @Column()
+    Tipo : string
+
+    @Column()
+    Cod01 : string
+
+    @Column()
+    Cod02 : string
+
+    @Column()
+    Descripcion : string
+
+    @Column({type : 'double'})
+    IdClienteProv : number
+
+    @Column()
+    IdSucursal : number
+
+    @Column()
+    Estado : string
+
+    @Column()
+    IdUsuario : number
+
+    @Column()
+    Usuario : string
+
+    @Column()
+    deleted_at : Date
+
+    @Column()
+    created_at : Date
+
+    @Column()
+    updated_at : Date
+
+
+
+
+
+    @OneToOne( () => ClienteModel , (Cliente) => Cliente.dCliente10 )
+    @JoinColumn({ name : 'IdClienteProv' , referencedColumnName : 'IdClienteProv' })
+    dCliente10 : ClienteModel
+
+
+    @OneToOne(() => SucursalModel , (photo) => photo.dLocal10 )
+    @JoinColumn({ name : 'IdSucursal' , referencedColumnName : 'IdSucursal' })
+    dLocal10 : SucursalModel
+
+}
+
+
 // ========================== DOT
 
-/**
+
 
 // ************************************************
 
@@ -86,6 +158,103 @@ constructor(
 // ======================================== SERVICE
 
 
+// ...................................................................
+
+  constructor(
+    @InjectRepository( DatosModel )private readonly datosModel:Repository<DatosModel> ,
+    private regEmple: RegistroEmpleadosService , 
+    private util:UtilidadesService , 
+  ){}
+
+  // ...................................................................
+  // ...................................................................
+
+  async findAll() {
+    
+    let data = await this.datosModel.find({
+      take : 200 ,
+      order : {
+        id : 'DESC'
+      }
+    });
+
+    return {
+      data , 
+      version : '1'
+    }
+
+  }
+
+  // ...................................................................
+
+  async findOne( uuid : string ) {
+    let data = await this.datosModel.findOne({
+      where : {
+        uu_id : uuid
+      }
+    });
+
+    return {
+      data , 
+      version : '1'
+    }
+  }
+
+  // ...................................................................
+
+  async update( uuid : string , dto : UpdateDatoDto ) {
+    
+    await this.datosModel.update({ uu_id : uuid } , dto );
+
+    let dataP = await this.datosModel.findOne({
+      where : {
+        uu_id : uuid
+      }
+    });
+
+    return {
+      data : dataP , 
+      version : '1'
+    }
+
+  }
+
+  // ...................................................................
+
+  async remove( uuid : string ) {
+    const updatedAt = moment().format('YYYY-MM-DD HH:mm:ss');
+
+    await this.datosModel.update({ uu_id : uuid } , { Estado : 'Anulado' , deleted_at : updatedAt , updated_at : updatedAt } );
+    let data = await this.datosModel.findOne({
+      where : {
+        uu_id : uuid
+      }
+    });
+
+    return {
+      data , 
+      version : '1'
+    }
+  }
+
+  // ...................................................................
+  // ...................................................................
+  async maxId()
+  {
+    let MaxId = await this.areaModel.createQueryBuilder('areas').select("MAX(areas.CodArea)", "max").getRawOne();
+    return MaxId.max + 1;
+  }
+  // ...................................................................
+  async buscaDescri( nombre :string )
+  {
+    let data = await this.areaModel.find({
+      where : {
+        Descripcion : nombre.trim()
+      }
+    });
+    return data.length;
+  }
+  // ...................................................................
 
 
 
@@ -93,8 +262,90 @@ constructor(
 
 
 
+// ======================================== CONTROLLER
 
 
+import * as moment from 'moment';
+import 'moment/locale/pt-br';
+import { v4 as uuidv4 } from 'uuid';
+import { JwtGuardGuard } from 'src/guard/jwt-guard/jwt-guard.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { Request } from 'express';
+import { UtilidadesService } from 'src/utilidades/utilidades.service';
+
+
+
+
+@ApiTags('Datos')
+@ApiBearerAuth()
+@UsePipes( new ValidationPipe )
+
+
+
+// ................................................................
+
+  constructor(
+    private readonly datosService: DatosService , 
+    private readonly util : UtilidadesService , 
+  ) {}
+
+  // ................................................................
+
+  @Post()
+  @HttpCode(200)
+  async create(@Body() dto : CreateDatoDto , @Req() req : Request ) {
+    
+    const createdAt = moment(  ).format('YYYY-MM-DD HH:mm:ss');
+    let headerToken = req.headers.authorization;
+    let Usuario = '' , IdUsuario = 0;
+
+    if( headerToken ){
+      let arTOken = headerToken.split(' ');
+      let dataT = await this.util.decodificaToken( arTOken[1] );
+      if( dataT ){
+        Usuario   = dataT['name'];
+        IdUsuario = dataT['dni'];
+      }
+    }
+
+    const bodyProocolo = {
+      ...dto , 
+      created_at : createdAt
+    };
+
+    return this.datosService.create( bodyProocolo );
+  }
+
+  // ................................................................
+
+  @Get()
+  findAll() {
+    return this.datosService.findAll();
+  }
+
+  // ................................................................
+
+  @Get(':uuid')
+  findOne( @Param('uuid') uuid : string) {
+    return this.datosService.findOne( uuid );
+  }
+
+  // ................................................................
+
+  @Patch(':uuid')
+  update(@Param('uuid') uuid : string, @Body() updateDatoDto: UpdateDatoDto) {
+    return this.datosService.update( uuid , updateDatoDto);
+  }
+
+  // ................................................................
+
+  @Delete(':uuid')
+  remove(@Param('uuid') uuid : string) {
+    return this.datosService.remove( uuid );
+  }
+
+  // ................................................................
 
 
 
