0648fed640d77a62fa7aabd96f8530646e4bbd60
Dany DLC <dany@delacruz.pe>
2024-03-10 11:34:29 -0500

    token de auditoria en controladores

--- Nest/Servicio_Nest.ts
+++ Nest/Servicio_Nest.ts
@@ -78,6 +78,8 @@ export class DatosModel {
 
 // ************************************************
 
+  export class createUserDto{
+
     @ApiProperty({
         description : 'Tipo',
         default     : 'Monitoreo',
@@ -95,60 +97,10 @@ export class DatosModel {
     Descripcion : string
 
     // ************************************************
-
-
-
-// ======================================== CONTROLADOR
-
-
-import * as moment from 'moment';
-import 'moment/locale/pt-br';
-import { v4 as uuidv4 } from 'uuid';
-import { JwtGuardGuard } from 'src/guard/jwt-guard/jwt-guard.guard';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { Pagination } from 'nestjs-typeorm-paginate';
-import { Request } from 'express';
-import { UtilidadesService } from 'src/utilidades/utilidades.service';
-
-
-
-
-@ApiTags('Datos')
-@ApiBearerAuth()
-@UsePipes( new ValidationPipe )
-
-
-constructor(
-    private readonly datosService: DatosService , 
-    private readonly util : UtilidadesService , 
-  ) {}
-
-
-@Post()
-  @HttpCode(200)
-  async create(@Body() dto : CreateDatoDto , @Req() req : Request ) {
-    
-    const createdAt = moment(  ).format('YYYY-MM-DD HH:mm:ss');
-    let headerToken = req.headers.authorization;
-    let Usuario = '' , IdUsuario = 0;
-
-    if( headerToken ){
-      let arTOken = headerToken.split(' ');
-      let dataT = await this.util.decodificaToken( arTOken[1] );
-      if( dataT ){
-        Usuario   = dataT['name'];
-        IdUsuario = dataT['dni'];
-      }
-    }
-
-    const bodyProocolo = {
-      ...dto , 
-      created_at : createdAt
-    };
-
-    return this.datosService.create( bodyProocolo );
+  
   }
 
+  
 
 
 
@@ -306,7 +258,8 @@ import { Request } from 'express';
 import { UtilidadesService } from 'src/utilidades/utilidades.service';
 
 
-
+// Para activar el auth JwTokenAuth
+@UseGuards( JwtGuardGuard )
 
 @ApiTags('Datos')
 @ApiBearerAuth()
