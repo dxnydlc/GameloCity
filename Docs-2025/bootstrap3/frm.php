6fced13350bfe63135aed9cd6dbaeb309b82342c
Dxny <dany@delacruz.pe>
2025-05-22 16:55:02 -0500

    ggg

--- Docs-2025/bootstrap3/frm.php
+++ Docs-2025/bootstrap3/frm.php
@@ -18,6 +18,41 @@
 	</div>
 	<!-- /.box-body -->
 	<div class="box-footer">
+		<div class=" row " >
+			<div class=" col-lg-2 col-md-2 " >
+				<a href="#" class="btn btn-default btn-block cerrarFrame " ><i class="fa fa-sign-out"></i> Salir</a>
+				<hr>
+			</div>
+			<!-- ./col -->
+			<div class=" col-lg-2 col-md-2 " >
+				<a id="btnImprimirDoc" target="_blanck" href="#" class="btn btn-default btn-block                                                                                                                                                                                                                                                                                                                                                                                                                                                         " >
+					<i class="fa fa-print "></i> imprimir
+				</a>
+				<hr>
+			</div>
+			<!-- ./col -->
+			<div class=" col-lg-2 col-md-2 ">
+				<a href="#" class="btn btn-warning btn-block" id="btnVerAuth" >Auditoría</a>
+				<hr>
+			</div>
+			<!-- ./col -->
+			<div class=" col-lg-2 col-md-2 " >
+				<a href="#" class="btn btn-danger btn-block" id="btnAnularR" ><i class="fa fa-save"></i> Anular</a>
+				<hr>
+			</div>
+			<!-- ./col -->
+			<div class=" col-lg-2 col-md-2 " >
+				<a href="#" class="btn btn-success btn-block" id="btnAprobarR" ><i class="fa fa-save"></i> Aprobar</a>
+				<hr>
+			</div>
+			<!-- ./col -->
+			<div class=" col-lg-2 col-md-2 " >
+				<a href="#" class="btn btn-primary btn-block" id="btnGuardarR" ><i class="fa fa-save"></i> Guardar</a>
+				<hr>
+			</div>
+			<!-- ./col -->
+		</div>
+		<!-- ./row -->
 		<!-- FOOTER -->
 	</div>
 	<!-- /.box-footer -->
