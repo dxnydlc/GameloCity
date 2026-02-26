54d9dfcbe203dbf66480c18dd5b5661908719f6d
Dxny <dany@delacruz.pe>
2026-02-26 11:59:30 -0500

    crud-2026

---
+++ crud-2026/form.php
@@ -0,0 +1,152 @@
+
+
+
+<div class="box box-primary " >
+	<div class="box-header with-border">
+		<i class="fa fa-text-width"></i>
+		<h3 class="box-title">Eventos asistencia</h3>
+		<div class=" box-tools pull-right ">
+			<div class="btn btn-box-tool">
+				<a id="btnCerrarTop" href="#" class=" cerrarFrame " ><i class="fa fa-times"></i> Cerrar</a>
+			</div>
+		</div>
+		<!-- /.box-tools -->
+	</div>
+	<!-- /.box-header -->
+	<div class="box-body">
+		
+        <!-- BODY -->
+        <form id="frmDocumento" autocomplete="off" >
+            
+            <input type="hidden" id="id" name="id" value="" />
+            <input type="hidden" id="uu_id" name="uu_id" value="" />
+            
+
+            <div class=" row " style="margin-bottom:15px;" >
+                <div class=" col-lg-2 col-md-2 " >
+                    <div class="form-group">
+                        <label for="Codigo" >Código</label> 
+                        <input type="text" name="Codigo" id="Codigo" class="form-control" value="" maxlength="150" />
+                    </div>
+                    <!-- ./form-group -->
+                </div>
+                <!-- ./col -->
+                <div class=" col-lg-4 col-md-4 " ></div>
+                <!-- ./col -->
+                <div class=" col-lg-3 col-md-3 " ></div>
+                <!-- ./col -->
+                <div class=" col-lg-3 col-md-3 " ></div>
+                <!-- ./col -->
+            </div>
+            <!-- ./row -->
+
+            <div class=" row " style="margin-bottom:15px;" >
+                <div class=" col-lg-6 col-md-6 " >
+                    <div class=" form-group ">
+                        <label for="IdClienteProv">Cliente</label>
+                        <select name="IdClienteProv" id="IdClienteProv" class="form-control" ></select>
+                        <a href="#" id="QuitarSelect">Quitar Seleccion</a>
+                    </div>
+                    <!-- ./form-group -->
+                </div>
+                <!-- ./col -->
+                <div class=" col-lg-6 col-md-6 " >
+                    <div class=" form-group ">
+                        <label for="IdSucursal">Sucursal</label>
+                        <select name="IdSucursal" id="IdSucursal" class="form-control" ></select>
+                    </div>
+                    <!-- ./form-group -->
+                </div>
+                <!-- ./col -->
+            </div>
+            <!-- ./row -->
+
+            <div class=" row " style="margin-bottom:15px;" >
+                <div class=" col-lg-6 col-md-6 " >
+                    <div class=" form-group ">
+                        <label for="Supervisor" >Personal</label>
+                        <select name="Supervisor" id="Supervisor" class="form-control" ></select>
+                    </div>
+                    <!-- ./form-group -->
+                </div>
+                <!-- ./col -->
+                <div class=" col-lg-6 col-md-6 " >
+                    <div class=" form-group ">
+                        <label for="IdArticulo" >Articulo</label>
+                        <select name="IdArticulo" id="IdArticulo" class="form-control" ></select>
+                    </div>
+                    <!-- ./form-group -->
+                </div>
+                <!-- ./col -->
+            </div>
+            <!-- ./row -->
+
+            <div class=" row " style="margin-bottom:15px;" >
+                <div class=" col-lg-3 col-md-3 " ></div>
+                <!-- ./col -->
+                <div class=" col-lg-3 col-md-3 " ></div>
+                <!-- ./col -->
+                <div class=" col-lg-3 col-md-3 " ></div>
+                <!-- ./col -->
+                <div class=" col-lg-3 col-md-3 " ></div>
+                <!-- ./col -->
+            </div>
+            <!-- ./row -->
+
+            <div class=" row " style="margin-bottom:15px;" >
+                <div class=" col-lg-3 col-md-3 " ></div>
+                <!-- ./col -->
+                <div class=" col-lg-3 col-md-3 " ></div>
+                <!-- ./col -->
+                <div class=" col-lg-3 col-md-3 " ></div>
+                <!-- ./col -->
+                <div class=" col-lg-3 col-md-3 " ></div>
+                <!-- ./col -->
+            </div>
+            <!-- ./row -->
+
+        </form>
+
+	</div>
+	<!-- /.box-body -->
+	<div class="box-footer">
+		<div class=" row " >
+			<div class=" col-lg-2 col-md-2 " >
+				<a href="#" class="btn btn-default btn-block cerrarFrame " ><i class="fa fa-sign-out"></i> Salir</a>
+				<hr>
+			</div>
+			<!-- ./col -->
+			<div class=" col-lg-2 col-md-2 " >
+				<a id="btnImprimirDoc" target="_blanck" href="#" class="btn btn-default btn-block                                                                                                                                                                                                                                                                                                                                                                                                                                                         " >
+					<i class="fa fa-print "></i> imprimir
+				</a>
+				<hr>
+			</div>
+			<!-- ./col -->
+			<div class=" col-lg-2 col-md-2 ">
+                <button class="btn btn-warning btn-block" id="btnVerAuth" ><div class="fa fa-clock-o"></div> Auditoría</button>
+				<hr>
+			</div>
+			<!-- ./col -->
+			<div class=" col-lg-2 col-md-2 " >
+                <button  class="btn btn-danger btn-block" id="btnAnularR" ><i class="fa fa-save"></i> Anula</button>
+				<hr>
+			</div>
+			<!-- ./col -->
+			<div class=" col-lg-2 col-md-2 " >
+                <button  class="btn btn-success btn-block" id="btnAprobarR" ><i class="fa fa-save"></i> Aprobar</button>
+				<hr>
+			</div>
+			<!-- ./col -->
+			<div class=" col-lg-2 col-md-2 " >
+                <button  class="btn btn-primary btn-block" id="btnGuardarR" ><i class="fa fa-save"></i> Guardar</button>
+				<hr>
+			</div>
+			<!-- ./col -->
+		</div>
+		<!-- ./row -->
+		<!-- FOOTER -->
+	</div>
+	<!-- /.box-footer -->
+</div>
+<!-- ./box -->
---
+++ crud-2026/home.php
@@ -0,0 +1,166 @@
+
+
+<?= $this->extend('layout/principal') ?>
+
+
+
+<!-- ************************************************** -->
+<?= $this->section('titulo') ?>
+Dashboard
+<?= $this->endSection() ?>
+<!-- ************************************************** -->
+
+
+
+
+
+<!-- ************************************************** -->
+<?= $this->section('header') ?>
+SSAYS
+<?= $this->endSection() ?>
+<!-- ************************************************** -->
+
+
+
+
+
+<!-- ************************************************** -->
+<?= $this->section('los_css') ?>
+
+<link rel="stylesheet" href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/autofill/2.7.0/css/autoFill.dataTables.min.css">
+<!-- botones -->
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.2.2/css/buttons.dataTables.min.css">
+<!-- ColReorder -->
+<link rel="stylesheet" href="https://cdn.datatables.net/colreorder/2.0.4/css/colReorder.dataTables.min.css">
+
+<!-- keytabable -->
+<link rel="stylesheet" href="https://cdn.datatables.net/keytable/2.12.1/css/keyTable.dataTables.min.css">
+<!-- Responsive -->
+<link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.4/css/responsive.dataTables.min.css">
+
+
+
+<?= $this->endSection() ?>
+<!-- ************************************************** -->
+
+
+
+
+
+<!-- ************************************************** -->
+<?= $this->section('content') ?>
+
+<!-- ============================================================== -->
+
+
+
+
+<section class="content-header">
+    <h1>
+        Asistencia eventos
+        <small>todos los documentos generados</small>
+    </h1>
+</section>
+
+
+<!-- Main content -->
+<section class="content"  >
+    
+    <div class=" row " id="wrapper_home" >
+        <!-- ./col -->
+        <div class=" col-lg-12 col-md-12 " >
+            <div class="box box-success " >
+            <div class="box-header with-border">
+                <h3 class="box-title">Resultado</h3>
+            </div>
+                <!-- /.box-header -->
+                <div class="box-body" >
+                    <div class="row">
+                        <div class=" col-lg-8 col-md-8 " ></div>
+                        <!-- ./col -->
+                         <div class=" col-lg-2 col-md-2 " >
+                            <a href="#" class="btn btn-primary btn-block pull-left " id="btnOpen_SRC" ><i class="fa fa-search"></i> Filtro</a>
+                            <hr>
+                        </div>
+                        <!-- ./col -->
+                        <div class=" col-lg-2 col-md-2 " >
+                            <a id="btnCrear" href="#" class="btn btn-primary btn-block pull-right " data-id="0" ><i class="icofont-plus"></i> Crear</a>
+                            <hr>
+                        </div>
+                        <!-- ./col -->
+                    </div>
+                    <hr>
+                    <table id="TablaHomePs" class=" table table-hover cell-border compact hover nowrap row-border table-striped table-hover " cellspacing="0" width="100%" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th></th>
+                                <th>Nro</th>
+                                <th>Descripción</th>
+                                <th>Estado</th>
+                                <th>Actualizado</th>
+                            </tr>
+                        </thead> 
+                        <tbody></tbody>
+                    </table>
+                </div>
+            <!-- ./box-body -->
+            <div class="box-footer" >
+                <div id="botones"></div>
+            </div>
+            <!-- ./box-footer -->
+            </div>
+        </div>
+        <!-- ./col -->
+    </div>
+    <!-- ./row -->
+
+
+
+    <div id="wrapper_form" style="display:none;" >
+        <?= $this->include( '/asistencias_25/asistencia_eventos/frmAEventos' ) ?>
+    </div>
+    
+
+</section>
+
+
+
+<!-- ============================================================== -->
+
+<?= $this->endSection() ?>
+<!-- ************************************************** -->
+
+
+
+
+
+<!-- ************************************************** -->
+<?= $this->section('los_js') ?>
+
+
+
+<script type="text/javascript" src="https://cdn.datatables.net/2.2.2/js/dataTables.min.js" ></script>
+<script type="text/javascript" src="https://cdn.datatables.net/autofill/2.7.0/js/dataTables.autoFill.min.js" ></script>
+<!-- botones -->
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/3.2.2/js/dataTables.buttons.min.js" ></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/3.2.2/js/buttons.colVis.min.js" ></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/3.2.2/js/buttons.print.min.js" ></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/3.2.2/js/buttons.html5.min.js" ></script>
+<!-- ColReorder -->
+<script type="text/javascript" src="https://cdn.datatables.net/colreorder/2.0.4/js/dataTables.colReorder.min.js" ></script>
+<!-- keytable -->
+<script type="text/javascript" src="https://cdn.datatables.net/keytable/2.12.1/js/dataTables.keyTable.min.js" ></script>
+<!-- Responsive -->
+<script type="text/javascript" src="https://cdn.datatables.net/responsive/3.0.4/js/dataTables.responsive.min.js" ></script>
+
+
+
+
+
+
+<script type="text/javascript" src="<?= esc($APP_URL) ?>librerias/asistencias_25/asistencia_eventos/homeAEventos.js?v=<?= $VERSION ?>" ></script>
+
+<?= $this->endSection() ?>
+<!-- ************************************************** -->
